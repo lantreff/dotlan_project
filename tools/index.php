a6d52a67a11bd1067c43c08794576d9aff6637a6
Torsten Amshove <torsten@amshove.net>
2014-01-12 20:52:57 +0100

    Erste Tools erstellt: Freeze & Platzzetteldruck

---
+++ tools/index.php
@@ -0,0 +1,20 @@
+<?php
+require_once("../../../global.php");
+require_once("../functions.php");
+
+$PAGE->sitetitle = $PAGE->htmltitle = _("Tools");
+
+if(!$DARF_PROJEKT_VIEW) $PAGE->error_die($HTML->gettemplate("error_rechtesystem"));
+else{
+  $output .= "<h3>Zettel drucken</h3>";
+  $output .= "<ul>";
+  $output .= "  <li><a href='platzzettel' target='_blank'>Platzzettel</a></li>";
+  $output .= "  <li><a href='verlosungszettel' target='_blank'>Verlosungszettel</a></li>";
+  $output .= "</ul>";
+  $output .= "<h3>Sonstiges</h3>";
+  $output .= "<ul>";
+  $output .= "  <li><a href='freeze'>Freeze</a> (DB-Funktionen die vor/nach dem Einspielen ins Intranet ausgef&uuml;hrt werden m&uuml;ssen)</li>";
+  $output .= "</ul>";
+}
+$PAGE->render($output);
+?>
