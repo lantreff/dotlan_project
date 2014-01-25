9eba772f8a7b377d192fc1f57655db5d5e2f6592
Torsten Amshove <torsten@amshove.net>
2014-01-25 22:03:09 +0100

    Auf neues Rechtesystem angepasst

--- webmail/index.php
+++ webmail/index.php
@@ -2,18 +2,18 @@
 #
 # session_name in der squirrel-config muss der gleiche sein wie der von dotlan - herausfinden kann man das mit der Funktion session_name();
 #
+$MODUL_NAME = "webmail";
 require_once("../../../global.php");
 require_once("../config.php");
 require_once("../functions.php");
 
 $PAGE->sitetitle = $PAGE->htmltitle = _("Webmail");
 
-if(!$DARF_PROJEKT_VIEW) $PAGE->error_die($HTML->gettemplate("error_rechtesystem"));
-else{
-  $_SESSION["webmail"]["user"] = $webmail_user;
-  $_SESSION["webmail"]["pw"] = $webmail_pw;
-  
-  $output .= "<iframe style='width: 99%; height: 600px; border: 0;' src='/admin/projekt/webmail/squirrelmail/src/redirect.php?autologin=true'></iframe>";
-}
+if(!$DARF["view"]) $PAGE->error_die($HTML->gettemplate("error_nopermission"));
+
+$_SESSION["webmail"]["user"] = $webmail_user;
+$_SESSION["webmail"]["pw"] = $webmail_pw;
+$output .= "<iframe style='width: 99%; height: 600px; border: 0;' src='/admin/projekt/webmail/squirrelmail/src/redirect.php?autologin=true'></iframe>";
+
 $PAGE->render($output);
 ?>
