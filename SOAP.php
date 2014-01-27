438d0b3f4dbeb5c32803b94d23f7ce36858506fa
Torsten <torsten@amshove.net>
2014-01-27 20:48:34 +0100

    Erste Vorbereitung fuer Router

--- SOAP.php
+++ SOAP.php
@@ -1,4 +1,6 @@
 <?php
+$soap_module = array("mx_router","gameserver_webinterface");
+
 $soap_url = "http://".$_SERVER['HTTP_HOST']."/admin/projekt/SOAP.php";
 $wsdl_funktionen = array(
   "getMe" => array(
@@ -8,7 +10,12 @@ $wsdl_funktionen = array(
   ),
   "getRechte" => array(
     "parameter" => array(
-      "modul_name" => "string",
+    ),
+    "return" => "array",
+  ),
+  "getUserById" => array(
+    "parameter" => array(
+      "id" => "int",
     ),
     "return" => "array",
   ),
@@ -67,8 +74,10 @@ if(isset($_GET["wsdl"])){
   
   $logged_in = false;
   $user_id = 0;
+  $event_id = $EVENT->next;
+  $MODUL_NAME = $_SERVER['HTTP_USER_AGENT'];
   
-  if(isset($_SERVER['PHP_AUTH_USER'])){
+  if(in_array($MODUL_NAME,$soap_module) && isset($_SERVER['PHP_AUTH_USER'])){
     $id = $DB->query_one("SELECT id FROM user WHERE nick = '".mysql_real_escape_string($_SERVER['PHP_AUTH_USER'])."' AND passwort = '".md5($_SERVER['PHP_AUTH_PW'])."' LIMIT 1");
     if($id){
       $user_id = $id;
@@ -91,13 +100,13 @@ if(isset($_GET["wsdl"])){
     }
       
     ####
-    # Gibt die Rechte des angemeldeten Users fuer das angegebene Modul zurueck
+    # Gibt die Rechte des angemeldeten Users zurueck
     ####
-    function getRechte($modul_name){
-      global $DB, $user_id;
+    function getRechte(){
+      global $DB, $user_id, $MODUL_NAME;
 
       $rechte = array();
-      $query = $DB->query("SELECT * FROM project_rights_rights WHERE bereich = '".$modul_name."'");
+      $query = $DB->query("SELECT * FROM project_rights_rights WHERE bereich = '".$MODUL_NAME."'");
       while($row = $DB->fetch_array($query)){
         $query2 = $DB->query("SELECT * FROM project_rights_user_rights WHERE user_id = '".$user_id."' AND right_id = '".$row["id"]."' LIMIT 1");
         if($DB->num_rows($query2) < 1) $rechte[$row["recht"]] = false;
@@ -105,11 +114,26 @@ if(isset($_GET["wsdl"])){
       }
       return $rechte;
     }
+    $DARF = getRechte();
+
+    ####
+    # Gibt Informationen zu einem User zueruck - Suche per user_id
+    ####
+    function getUserById($id){
+      global $DB, $DARF, $MODUL_NAME, $event_id;
+
+      if($MODUL_NAME == "mx_router" && $DARF["view"]){
+        $user = array();
+        $user = $DB->query_first("SELECT nick, vorname, nachname FROM user WHERE id ='".mysql_real_escape_string($id)."' LIMIT 1");
+        $event = $DB->query_first("SELECT sitz_nr FROM event_teilnehmer WHERE user_id = '".mysql_real_escape_string($id)."' AND event_id = '".$event_id."'");
+        if(is_array($event)) $user = array_merge($user,$event);
+        return $user;
+      }else return false;
+    }
 
     ## Funktionen registrieren
     $server = new SoapServer($soap_url."?wsdl");
-    $server->addFunction("getMe");
-    $server->addFunction("getRechte");
+    foreach($wsdl_funktionen as $fkt => $val) $server->addFunction($fkt);
     $server->handle();
   }
 }
